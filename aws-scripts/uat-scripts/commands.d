76ee5ee9aa12d7bfa0d598f3269ea7f1ccf85f67
Bob Sakawa <bob.sakawa@gmail.com>
2022-06-23 20:40:49 +0300

    prod setup

---
+++ aws-scripts/uat-scripts/commands.d
@@ -0,0 +1,27 @@
+
+
+ssh-add jump-box.cer
+
+ssh -A -i "jump-box.cer" ec2-user@ec2-54-218-236-102.us-west-2.compute.amazonaws.com
+
+ssh ec2-user@10.0.2.32
+ssh ec2-user@10.0.2.231
+
+service codedeploy-agent status
+
+
+s3://asg-webappdeploymentbucket-1737johkumm3m/Deploy-HTML-master.zip
+
+
+s3://asg-webappdeploymentbucket-1nlar8yn0nbjr/Archive.zip
+
+
+
+telnet mysqldb.chy8dvhamnmp.us-west-2.rds.amazonaws.com 3306
+
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_DEFAULT_REGION
+S3_BUCKET
+APPLICATION_NAME
+DEPLOYMENT_GROUP
